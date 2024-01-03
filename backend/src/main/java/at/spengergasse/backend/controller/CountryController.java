01e822911ce33324003862c8b1a199ae58b347ee
wac22585 <wac22585@spengergasse.at>
2024-01-03 22:37:24 +0100

    controller for country numbers.

--- backend/src/main/java/at/spengergasse/backend/controller/CountryController.java
+++ backend/src/main/java/at/spengergasse/backend/controller/CountryController.java
@@ -1,2 +1,26 @@
-package at.spengergasse.backend.controller;public class CountryController {
+package at.spengergasse.backend.controller;
+
+import at.spengergasse.backend.model.CountryNumber;
+import at.spengergasse.backend.persistence.CountryNumberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/countries")
+@CrossOrigin(origins = "http://localhost:3000")
+public class CountryController {
+
+    @Autowired
+    private CountryNumberRepository countryNumberRepository;
+
+    @GetMapping("/all")
+    public List<CountryNumber> getCountryCodes(){
+        return countryNumberRepository.findAll();
+    }
 }
